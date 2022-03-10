1a15aab7018e928bbeaebbb21000354552d7ddea
luis <>
2022-03-10 19:11:37 +0800

    prepare to tune paras

--- train_intent.py
+++ train_intent.py
@@ -31,55 +31,75 @@ def main(args):
 
     data_paths = {split: args.data_dir / f"{split}.json" for split in SPLITS}
     data = {split: json.loads(path.read_text()) for split, path in data_paths.items()}
+
+    embeddings = torch.load(args.cache_dir / "embeddings.pt")
+
+    # Parameters tuning
+
     datasets: Dict[str, SeqClsDataset] = {
         split: SeqClsDataset(split_data, vocab, intent2idx, args.max_len)
         for split, split_data in data.items()
     }
-    # TODO: crecate DataLoader for train / dev datasets
 
-    train=DataLoader(datasets['train'],batch_size=128, shuffle=False, collate_fn=lambda x: tuple(x_.to(device) for x_ in datasets['train'].collate_fn(x)))
-    evals=DataLoader(datasets['eval'],batch_size=128, shuffle=False, collate_fn=lambda x: tuple(x_.to(device) for x_ in datasets['eval'].collate_fn(x)))
+    evals=DataLoader(datasets['eval'],batch_size=args.batch_size, shuffle=False, collate_fn=lambda x: tuple(x_.to(device) for x_ in datasets['eval'].collate_fn(x)))
 
-    embeddings = torch.load(args.cache_dir / "embeddings.pt")
+    best_acc=0
+    for i in range(3):
+        train=DataLoader(datasets['train'],batch_size=args.batch_size*2**i, shuffle=False, collate_fn=lambda x: tuple(x_.to(device) for x_ in datasets['train'].collate_fn(x)))
 
-    # TODO: init model and move model to target device(cpu / gpu)
-    model = SeqClassifier(embeddings=embeddings,hidden_size=args.hidden_size,num_layers=args.num_layers,dropout=args.dropout,bidirectional=args.bidirectional,num_class=len(intent2idx))
-    model.to(device)
+        for j in range(3):
+            model = SeqClassifier(embeddings=embeddings,hidden_size=args.hidden_size,
+                                num_layers=args.num_layers,dropout=args.dropout*2**j,bidirectional=args.bidirectional,num_class=len(intent2idx))
+            model.to(device)
+            optimizer = optim.SGD(model.parameters(), lr=args.lr)
+            criterion = torch.nn.CrossEntropyLoss()
+            criterion.to(device)
 
-    # TODO: init optimizer
-    optimizer = optim.SGD(model.parameters(), lr=args.lr)
-    criterion = torch.nn.CrossEntropyLoss()
-    criterion.to(device)
+            epoch_pbar = trange(args.num_epoch, desc="Epoch")
+            old_acc=0
+            for epoch in epoch_pbar:
 
-    epoch_pbar = trange(args.num_epoch, desc="Epoch")
-    for epoch in epoch_pbar:
+                lr = args.lr * (0.2 ** (epoch // 20))
+                for param_group in optimizer.param_groups:
+                    param_group['lr'] = lr
 
-        model.train()
-        acc=0
-        n=0
-        for labels, texts in train:
+                model.train()
+                acc=0
+                n=0
+                for labels, texts in train:
 
-            out = model(texts)
-            p_labels = torch.argmax(out, dim=1)
+                    out = model(texts)
+                    p_labels = torch.argmax(out, dim=1)
 
-            loss = criterion(out, labels)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+                    loss = criterion(out, labels)
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
 
-            acc=acc+torch.sum(p_labels == labels)
-            n = n + len(labels)
+                    acc=acc+torch.sum(p_labels == labels)
+                    n = n + len(labels)
 
-        acc=acc.item()/n*100
+                acc=acc.item()/n*100
 
-        if epoch % 50 == 0:
-            print(f"Epoch: {epoch:5d}, Accuracy {acc:.4f}%")
-        # TODO: Training loop - iterate over train dataloader and update model weights
-        # TODO: Evaluation loop - calculate accuracy and save model weights
-        pass
+                if acc > 90 and (old_acc-acc)/acc*100 < 0.01 : break
+                old_acc=acc
 
-    # TODO: Inference on test set
+                if epoch % 50 == 0:
+                    print(f"Epoch: {epoch:5d}, Accuracy {acc:.4f}%")
 
+            model.eval()
+            for labels, texts in evals:
+                out = model(texts)
+                p_labels = torch.argmax(out, dim=1)
+                acc=acc+torch.sum(p_labels == labels)
+                n = n + len(labels)
+            acc=acc.item()/n*100
+
+            if acc > best_acc :
+                best_acc = acc
+                best_paras = model.state_dict()
+
+    torch.save(best_paras,args.ckpt_dir / "best_model.pth")
 
 def parse_args() -> Namespace:
     parser = ArgumentParser()
@@ -103,19 +123,19 @@ def parse_args() -> Namespace:
     )
 
     # data
-    parser.add_argument("--max_len", type=int, default=128)
+    parser.add_argument("--max_len", type=int, default=20)
 
     # model
     parser.add_argument("--hidden_size", type=int, default=256)
     parser.add_argument("--num_layers", type=int, default=2)
-    parser.add_argument("--dropout", type=float, default=0.1)
+    parser.add_argument("--dropout", type=float, default=0.05)
     parser.add_argument("--bidirectional", type=bool, default=True)
 
     # optimizer
-    parser.add_argument("--lr", type=float, default=1e-3)
+    parser.add_argument("--lr", type=float, default=10)
 
     # data loader
-    parser.add_argument("--batch_size", type=int, default=128)
+    parser.add_argument("--batch_size", type=int, default=32)
 
     # training
     parser.add_argument(
