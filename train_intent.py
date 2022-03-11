43f10da4b223876a25f129e365bcd32c49ac67ad
luis <>
2022-03-12 02:54:47 +0800

    add k fold

--- train_intent.py
+++ train_intent.py
@@ -9,8 +9,10 @@ from tqdm import trange
 
 from dataset import SeqClsDataset
 from utils import Vocab
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler
 from model import SeqClassifier
+
+from sklearn.model_selection import KFold
 
 import torch.optim as optim
 
@@ -31,40 +33,40 @@ def main(args):
 
     data_paths = {split: args.data_dir / f"{split}.json" for split in SPLITS}
     data = {split: json.loads(path.read_text()) for split, path in data_paths.items()}
+    data=[y for x in data.keys() for y in data[x] ]
 
     embeddings = torch.load(args.cache_dir / "embeddings.pt")
 
-    # Parameters tuning
-
-    datasets: Dict[str, SeqClsDataset] = {
-        split: SeqClsDataset(split_data, vocab, intent2idx, args.max_len)
-        for split, split_data in data.items()
-    }
-
-    evals=DataLoader(datasets['eval'],batch_size=args.batch_size, shuffle=False, collate_fn=lambda x: tuple(x_.to(device) for x_ in datasets['eval'].collate_fn(x)))
-    train=DataLoader(datasets['train'],batch_size=args.batch_size, shuffle=False, collate_fn=lambda x: tuple(x_.to(device) for x_ in datasets['train'].collate_fn(x)))
+    datasets = SeqClsDataset(data, vocab, intent2idx, args.max_len)
 
     with open(f"./{args.name}_result","w") as f:
         f.write(f">> Learning Rate: {args.lr}, max_len: {args.max_len}\n")
-    
-    best_acc=0
-    for j in range(1):
+
+    torch.manual_seed(12)
+    kf = KFold(n_splits=10)
+
+    fold=0
+    f_acc=0
+    for train_ind, test_ind in kf.split(datasets):
+
+        fold+=1
         model = SeqClassifier(embeddings=embeddings,hidden_size=args.hidden_size,
-                            num_layers=args.num_layers,dropout=args.dropout/2**j,bidirectional=args.bidirectional,num_class=len(intent2idx))
+                    num_layers=args.num_layers,dropout=args.dropout,bidirectional=args.bidirectional,num_class=len(intent2idx))
+
         model.to(device)
         #optimizer = optim.SGD(model.parameters(), lr=args.lr)
         optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr)
-        # scheduler = optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.1)
         criterion = torch.nn.CrossEntropyLoss()
         criterion.to(device)
 
-        # Train loop
-        _acc=0
+        train_loader=DataLoader(datasets,batch_size=128,shuffle=False,collate_fn=lambda x: tuple(x_.to(device) for x_ in datasets.collate_fn(x)),sampler=SubsetRandomSampler(train_ind))
+        test_loader=DataLoader(datasets,batch_size=128,shuffle=False,collate_fn=lambda x: tuple(x_.to(device) for x_ in datasets.collate_fn(x)),sampler=SubsetRandomSampler(test_ind))
+
         epoch_pbar = trange(args.num_epoch, desc="Epoch")
         for epoch in epoch_pbar:
 
             model.train()
-            for labels, texts in train:
+            for labels, texts in train_loader:
 
                 out = model(texts)
                 p_labels = torch.argmax(out, dim=1)
@@ -75,36 +77,29 @@ def main(args):
                 optimizer.step()
 
             model.eval()
-            acc=0
-            n=0
-            for labels, texts in evals:
+            acc=0;n=0
+            for labels, texts in test_loader:
                 out = model(texts)
                 p_labels = torch.argmax(out, dim=1)
                 acc=acc+torch.sum(p_labels == labels)
                 n = n + len(labels)
             acc=acc.item()/n*100
 
-            # if acc > _acc :
-            #     _acc = acc
-            # elif _acc > 60:
-            #     scheduler.step()
+            if epoch % 60 == 0:
+                print(f"\nFold: {fold}, Epoch: {epoch:5d}, Accuracy {acc:.4f}%")
 
-            if epoch % 30 == 0:
-                # print(f"\nEpoch: {epoch:5d}, Accuracy {acc:.4f}%, LR={scheduler.get_last_lr()[0]}")
-                print(f"\nEpoch: {epoch:5d}, Accuracy {acc:.4f}%")
+        f_acc += acc / kf.get_n_splits()
+        print('*'*40)
 
-        print("="*40)
-        print(f"Dropout: {args.dropout/2**j:.4f}, Accuracy: {acc:.4f}%")
-        with open(f"./{args.name}_result","a") as f:
-            f.write(f"Dropout: {args.dropout/2**j:.4f}, Accuracy: {acc:.4f}%.\n")
-        print("="*40)
-        if acc > best_acc :
-            best_acc = acc
-            best_paras = model.state_dict()
+    info=f"Dropout:{args.dropout:.4f}, hidden_size:{args.hidden_size:d}, layers:{args.num_layers:d}, batch_size:{args.batch_size:d}\n"
+    info+=f"Accuracy: {f_acc:.2f}%"
+    print("="*40)
+    with open(f"./{args.name}_result","a") as f:
+        f.write(f"{info}\n")
+    print("="*40)
 
-        torch.save(best_paras,args.ckpt_dir / f"./{args.name}_best_model.pth")
+    torch.save(model.state_dict(),args.ckpt_dir / f"./{args.name}_best_model.pth")
     
-
 def parse_args() -> Namespace:
     parser = ArgumentParser()
     parser.add_argument(
@@ -127,28 +122,28 @@ def parse_args() -> Namespace:
     )
 
     # data
-    parser.add_argument("--max_len", type=int, default=18)
+    parser.add_argument("--max_len", type=int, default=24)
 
     # model
-    parser.add_argument("--hidden_size", type=int, default=128)
+    parser.add_argument("--hidden_size", type=int, default=256)
     parser.add_argument("--num_layers", type=int, default=2)
-    parser.add_argument("--dropout", type=float, default=0.1)
+    parser.add_argument("--dropout", type=float, default=0.0)
     parser.add_argument("--bidirectional", type=bool, default=True)
 
     # optimizer
     parser.add_argument("--lr", type=float, default=1e-4)
 
     # data loader
-    parser.add_argument("--batch_size", type=int, default=128)
+    parser.add_argument("--batch_size", type=int, default=256)
 
     # training
     parser.add_argument(
         "--device", type=torch.device, help="cpu, cuda, cuda:0, cuda:1", default="cuda"
     )
-    parser.add_argument("--num_epoch", type=int, default=200)
+    parser.add_argument("--num_epoch", type=int, default=360)
 
     # output
-    parser.add_argument("--name", type=str, default="")
+    parser.add_argument("--name", type=str, default="default_name")
 
     args = parser.parse_args()
     return args
