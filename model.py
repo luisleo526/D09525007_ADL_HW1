03768feb3d0789e65d139f17f834549f17e89048
luis <>
2022-03-13 04:22:24 +0800

    123

--- model.py
+++ model.py
@@ -19,8 +19,6 @@ class SeqClassifier(torch.nn.Module):
         self.rnn = torch.nn.GRU( input_size=self.embed.embedding_dim, hidden_size=hidden_size, num_layers=num_layers, dropout=dropout,bidirectional=bidirectional)
         
         self.fc = torch.nn.Linear(hidden_size,num_class)
-        self.act = torch.nn.ReLU()
-        self.fcc = torch.nn.Linear(num_class,num_class)
 
     @property
     def encoder_output_size(self) -> int:
@@ -36,7 +34,5 @@ class SeqClassifier(torch.nn.Module):
 
         hidden = ht[-1,:,:]
         hidden = self.fc(hidden)
-        hidden = self.act(hidden)
-        hidden = self.fcc(hidden)
 
         return hidden
