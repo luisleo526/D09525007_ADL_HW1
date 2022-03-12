40cbc9a1bdbd14205ff07259beb8d9de0df5cb64
luis <>
2022-03-13 04:06:40 +0800

    123

--- model.py
+++ model.py
@@ -16,14 +16,11 @@ class SeqClassifier(torch.nn.Module):
         super(SeqClassifier, self).__init__()
         self.embed = Embedding.from_pretrained(embeddings, freeze=False)
 
-        # TODO: model architecture
-        #self.rnn = torch.nn.LSTM(input_size=self.embed.embedding_dim, hidden_size=hidden_size, num_layers=num_layers, dropout=dropout,bidirectional=bidirectional)
         self.rnn = torch.nn.GRU( input_size=self.embed.embedding_dim, hidden_size=hidden_size, num_layers=num_layers, dropout=dropout,bidirectional=bidirectional)
         
         self.fc = torch.nn.Linear(hidden_size,num_class)
-
-        # self.fc1 = torch.nn.Linear(hidden_size*2, hidden_size)
-        # self.fc2 = torch.nn.Linear(hidden_size, num_class)
+        self.act = torch.nn.ReLU()
+        self.fcc = torch.nn.Linear(num_class,num_class)
 
     @property
     def encoder_output_size(self) -> int:
@@ -35,14 +32,11 @@ class SeqClassifier(torch.nn.Module):
         x = self.embed(batch)
         x = pack_padded_sequence(x, [len(data) for data in x], batch_first=True)
 
-        #out_pack, (ht, ct) = self.rnn(x)
         out_pack, ht = self.rnn(x)
 
-        # hidden = torch.cat((ht[-2,:,:], ht[-1,:,:]), dim = 1)
-        # hidden = self.fc1(hidden)
-        # hidden = self.fc2(hidden)
-  
         hidden = ht[-1,:,:]
         hidden = self.fc(hidden)
+        hidden = self.act(hidden)
+        hidden = self.fcc(hidden)
 
         return hidden
